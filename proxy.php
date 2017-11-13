059089857bfdae798e110d8d4eec69d9ae97a875
Emanuele Toscano <toss82+git@gmail.com>
2017-11-13 13:18:26 +0100

    Better check for yahoo finance proxy

--- proxy.php
+++ proxy.php
@@ -1,8 +1,9 @@
 <?php
 $params = json_decode(file_get_contents('php://input'), true);
 if (isset($params['url'])) {
-    if (strpos($params['url'], 'https://finance.yahoo.com/_finance_doubledown/api') === false) {
+    if (strpos($params['url'], 'https://finance.yahoo.com/_finance_doubledown/api') !== 0) {
         // Only accept queries to yahoo finance API or die
+        header('HTTP/1.0 404 Not Found');
         die();
     }
     $data = file_get_contents($params['url']);
